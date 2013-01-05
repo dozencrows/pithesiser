76505be2d59147998408969c4b84c82ee5dea876
Nick Tuckett <git@magwitch.uk.net>
2013-01-05 14:16:34 +0000

    Dev: synth-test: wip on oscilloscope-style rendering: made waveform rendering more modular (instantiable).

--- gfx_wave_render.c
+++ gfx_wave_render.c
@@ -14,6 +14,35 @@
 #include "gfx_event_types.h"
 #include "VG/openvg.h"
 
+typedef struct waveform_renderer_def_t
+{
+	int	x, y;
+	int width, height;
+	int tuned_wavelength;
+	int amplitude_scale;
+	VGfloat	background_colour[4];
+	VGfloat	line_colour[4];
+	VGfloat line_width;
+	VGint line_cap_style;
+	VGint line_join_style;
+	VGint rendering_quality;
+} waveform_renderer_def_t;
+
+typedef struct waveform_renderer_state_t
+{
+	VGPaint line_paint;
+	int half_height;
+	int rendered_samples;
+	VGPath path;
+	int16_t last_sample;
+} waveform_renderer_state_t;
+
+typedef struct waveform_renderer_t
+{
+	waveform_renderer_def_t	definition;
+	waveform_renderer_state_t state;
+} waveform_renderer_t;
+
 //--------------------------------------------------------------------------------------------------------------
 // Common waveform rendering support
 
@@ -23,114 +52,146 @@
 
 #define VG_ERROR_CHECK(s)	{ VGErrorCode error = vgGetError(); if (error != VG_NO_ERROR) printf("VG Error: %d (%s)\n", error, s); }
 
-static int buffer_render_x	= 0;
+//--------------------------------------------------------------------------------------------------------------
+// Test data
+//
+
+waveform_renderer_t waveform_renderer =
+{
+	{
+		0, 256, 1024, 512,
+		1, 129,
+		{ 0.0f, 0.0f, 64.0f, 255.0f },
+		{ 0.0f, 255.0f, 0.0f, 255.0f },
+		1.0f,
+		VG_CAP_BUTT, VG_JOIN_MITER, VG_RENDERING_QUALITY_NONANTIALIASED
+	},
+	{
+		(VGPaint)0,
+		0,
+		0,
+		(VGPath)0,
+		0
+	}
+};
 
 //--------------------------------------------------------------------------------------------------------------
 // Vector-based waveform rendering
 //
 
-#define VECTOR_WAVE_WIDTH		1024
-#define VECTOR_WAVE_X			0
-#define VECTOR_WAVE_Y			256
-#define VECTOR_WAVE_HALFHEIGHT	256
-#define VECTOR_WAVE_SCALE		129		// roughly 32767 divided by 256 with a safety margin
+#define CALC_Y_COORD(renderer, sample)	(renderer->definition.y + (sample / renderer->definition.amplitude_scale))
 
-#define CALC_Y_COORD(sample)	(VECTOR_WAVE_Y + (sample / VECTOR_WAVE_SCALE))
-
-static int16_t last_sample = 0;
-static VGPath wave_path;
-static VGPaint wave_stroke_paint;
 static VGubyte first_sample_segment_commands[1] = { VG_MOVE_TO_ABS };
 static VGubyte horiz_segment_commands[3] = { VG_MOVE_TO_ABS, VG_VLINE_TO_ABS, VG_HLINE_TO_ABS };
 static VGubyte sample_segment_commands[128];
 static VGshort sample_segment_coords[128 * 2];
 
-static void vector_wave_event_handler(gfx_event_t *event)
+void initialise_renderer(waveform_renderer_t *renderer)
 {
-	size_t sample_count = event->size / BYTES_PER_SAMPLE;
-	int16_t *sample_data = (int16_t*) event->ptr;
+	renderer->state.line_paint = vgCreatePaint();
+	VG_ERROR_CHECK("vgCreatePaint");
+	vgSetParameteri(renderer->state.line_paint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
+	vgSetParameterfv(renderer->state.line_paint, VG_PAINT_COLOR, 4, renderer->definition.line_colour);
+	renderer->state.half_height = renderer->definition.height / 2;
+	renderer->state.path = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_S_16, 1.0f, 0.0f, 0, 0, VG_PATH_CAPABILITY_APPEND_TO);
+	VG_ERROR_CHECK("vgCreatePath");
+}
 
-	if (buffer_render_x < VECTOR_WAVE_WIDTH)
+void deinitialise_renderer(waveform_renderer_t *renderer)
+{
+	vgDestroyPath(renderer->state.path);
+	vgDestroyPaint(renderer->state.line_paint);
+}
+
+void render_samples(waveform_renderer_t *renderer, size_t sample_count, int16_t *sample_data)
+{
+	if (renderer->state.rendered_samples < renderer->definition.width)
 	{
-		if (buffer_render_x == 0)
+		if (renderer->state.rendered_samples == 0)
 		{
 			VGshort segment_coords[2];
-			segment_coords[0] = VECTOR_WAVE_X;
-			segment_coords[1] = CALC_Y_COORD(last_sample);
+			segment_coords[0] = renderer->definition.x;
+			segment_coords[1] = CALC_Y_COORD(renderer, renderer->state.last_sample);
 
-			vgAppendPathData(wave_path, 1, first_sample_segment_commands, segment_coords);
+			vgAppendPathData(renderer->state.path, 1, first_sample_segment_commands, segment_coords);
 			VG_ERROR_CHECK("vgAppendPathData - wave 1");
 		}
 
 		VGshort *coord_ptr = sample_segment_coords;
 		int16_t *sample_ptr = sample_data;
 		size_t coord_count = sample_count;
-		while (coord_count > 0 && buffer_render_x < VECTOR_WAVE_WIDTH)
+		while (coord_count > 0 && renderer->state.rendered_samples < renderer->definition.width)
 		{
-			*coord_ptr++ = VECTOR_WAVE_X + buffer_render_x;
-			*coord_ptr++ = CALC_Y_COORD(*sample_ptr);
+			*coord_ptr++ = renderer->definition.x + renderer->state.rendered_samples;
+			*coord_ptr++ = CALC_Y_COORD(renderer, *sample_ptr);
 
 			sample_ptr += CHANNELS_PER_SAMPLE;
-			buffer_render_x++;
+			renderer->state.rendered_samples++;
 			coord_count--;
 		}
 
-		vgAppendPathData(wave_path, sample_count - coord_count, sample_segment_commands, sample_segment_coords);
+		vgAppendPathData(renderer->state.path, sample_count - coord_count, sample_segment_commands, sample_segment_coords);
 		VG_ERROR_CHECK("vgAppendPathData - wave 2");
 	}
 
-	last_sample = sample_data[(sample_count - 1) * CHANNELS_PER_SAMPLE];
+	renderer->state.last_sample = sample_data[(sample_count - 1) * CHANNELS_PER_SAMPLE];
+}
+
+void render_silence(waveform_renderer_t *renderer, size_t sample_count)
+{
+	if (renderer->state.rendered_samples < renderer->definition.width)
+	{
+		VGshort segment_coords[4];
+		segment_coords[0] = renderer->definition.x + renderer->state.rendered_samples;
+		segment_coords[1] = CALC_Y_COORD(renderer, renderer->state.last_sample);
+		segment_coords[2] = CALC_Y_COORD(renderer, 0);
+        segment_coords[3] = renderer->definition.x + MIN(renderer->state.rendered_samples + sample_count, renderer->definition.width);
+		vgAppendPathData(renderer->state.path, 3, horiz_segment_commands, segment_coords);
+		VG_ERROR_CHECK("vgAppendPathData - silence");
+
+		renderer->state.rendered_samples += sample_count;
+	}
+
+	renderer->state.last_sample = 0;
+}
+
+void update_display(waveform_renderer_t *renderer)
+{
+	vgSetfv(VG_CLEAR_COLOR, 4, renderer->definition.background_colour);
+	vgClear(renderer->definition.x, renderer->definition.y - renderer->state.half_height,
+			renderer->definition.width + 1, renderer->definition.height);
+
+	vgSetf(VG_STROKE_LINE_WIDTH, renderer->definition.line_width);
+	vgSeti(VG_STROKE_CAP_STYLE, renderer->definition.line_cap_style);
+	vgSeti(VG_STROKE_JOIN_STYLE, renderer->definition.line_join_style);
+	vgSetPaint(renderer->state.line_paint, VG_STROKE_PATH);
+	vgSeti(VG_RENDERING_QUALITY, renderer->definition.rendering_quality);
+	vgDrawPath(renderer->state.path, VG_STROKE_PATH);
+	VG_ERROR_CHECK("vgDrawPath");
+	vgClearPath(renderer->state.path, VG_PATH_CAPABILITY_APPEND_TO);
+	VG_ERROR_CHECK("vgClearPath");
+	renderer->state.rendered_samples = 0;
+}
+
+static void vector_wave_event_handler(gfx_event_t *event)
+{
+	render_samples(&waveform_renderer, event->size / BYTES_PER_SAMPLE, (int16_t*)event->ptr);
 	free(event->ptr);
 }
 
 static void vector_silence_event_handler(gfx_event_t *event)
 {
-	if (buffer_render_x < VECTOR_WAVE_WIDTH)
-	{
-		size_t sample_count = event->size / BYTES_PER_SAMPLE;
-
-		VGshort segment_coords[4];
-		segment_coords[0] = VECTOR_WAVE_X + buffer_render_x;
-		segment_coords[1] = CALC_Y_COORD(last_sample);
-		segment_coords[2] = CALC_Y_COORD(0);
-        segment_coords[3] = VECTOR_WAVE_X + MIN(buffer_render_x + sample_count, VECTOR_WAVE_WIDTH);
-		vgAppendPathData(wave_path, 3, horiz_segment_commands, segment_coords);
-		VG_ERROR_CHECK("vgAppendPathData - silence");
-
-        buffer_render_x += sample_count;
-	}
-
-	last_sample = 0;
+	render_silence(&waveform_renderer, event->size / BYTES_PER_SAMPLE);
 }
 
 static void vector_swap_event_handler(gfx_event_t *event)
 {
-	static VGfloat clear_colour[] = { 0.0f, 0.0f, 64.0f, 1.0f };
-
-	vgSetfv(VG_CLEAR_COLOR, 4, clear_colour);
-	vgClear(VECTOR_WAVE_X, VECTOR_WAVE_Y - VECTOR_WAVE_HALFHEIGHT, VECTOR_WAVE_WIDTH + 1, VECTOR_WAVE_HALFHEIGHT * 2);
-
-	vgSetf(VG_STROKE_LINE_WIDTH, 1.0f);
-	vgSeti(VG_STROKE_CAP_STYLE, VG_CAP_BUTT);
-	vgSeti(VG_STROKE_JOIN_STYLE, VG_JOIN_MITER);
-	vgSetPaint(wave_stroke_paint, VG_STROKE_PATH);
-	vgSeti(VG_RENDERING_QUALITY, VG_RENDERING_QUALITY_NONANTIALIASED);
-	vgDrawPath(wave_path, VG_STROKE_PATH);
-	VG_ERROR_CHECK("vgDrawPath");
-	vgClearPath(wave_path, VG_PATH_CAPABILITY_APPEND_TO);
-	VG_ERROR_CHECK("vgClearPath");
-	buffer_render_x = 0;
+	update_display(&waveform_renderer);
 }
 
 static void vector_post_init_handler(gfx_event_t *event)
 {
-	wave_path = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_S_16, 1.0f, 0.0f, 0, 0, VG_PATH_CAPABILITY_APPEND_TO);
-	VG_ERROR_CHECK("vgCreatePath");
-
-	VGfloat line_colour[4] = { 0.0f, 255.0f, 0.0f, 255.0f };
-	wave_stroke_paint = vgCreatePaint();
-	vgSetParameteri(wave_stroke_paint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
-	vgSetParameterfv(wave_stroke_paint, VG_PAINT_COLOR, 4, line_colour);
+	initialise_renderer(&waveform_renderer);
 }
 
 void gfx_wave_render_vector_initialise()
@@ -140,16 +201,12 @@ void gfx_wave_render_vector_initialise()
 	gfx_register_event_handler(GFX_EVENT_SILENCE, vector_silence_event_handler);
 	gfx_register_event_handler(GFX_EVENT_BUFFERSWAP, vector_swap_event_handler);
 
-	buffer_render_x = 0;
-	last_sample = 0;
-
 	memset(sample_segment_commands, VG_LINE_TO_ABS, sizeof(sample_segment_commands));
 }
 
 void gfx_wave_render_vector_deinitialise()
 {
-	vgDestroyPath(wave_path);
-	vgDestroyPaint(wave_stroke_paint);
+	deinitialise_renderer(&waveform_renderer);
 }
 
 //--------------------------------------------------------------------------------------------------------------
