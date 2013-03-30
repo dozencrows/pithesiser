6edfd6341c721b28844a9ac1c0d360915c2e1ac2
Nick Tuckett <git@magwitch.uk.net>
2013-03-30 17:25:49 +0000

    Dev: synth-test: wip on filter improvements: tweaks to further optimise asm filter loop, and make it more readable.

--- filter_arm.asm
+++ filter_arm.asm
@@ -25,27 +25,26 @@ filter_apply_asm:
 		str		r2,[sp]
 		b		.L1
 
-.L0:	mov		r6, r5									@ o[1] = o[0]
+.L0:
 		mov		r4, r3									@ h[1] = h[0]
-		mov		r5, r7									@ o[0] = last output
+		mov		r6, r5									@ o[1] = o[0]
 		mov		r3, r2									@ h[0] = last input
+		mov		r5, r7									@ o[0] = last output
 
-.L1:	mul		r7,r10,r4								@ h[1] * ic[2]
-
+.L1:
 		ldrsh	r2,[r0]									@ load new sample
-		subs	r1,r1,#1								@ decrement overall count (pipelining)
-
-		mla		r7,r2,r8,r7								@ += sample * ic[0]
+		subs	r1,r1,#1								@ decrement overall count (here to avoid stall from 16-bit load above)
+		mul		r7,r10,r4								@ h[1] * ic[2]			  (helps to avoid stall from 16-bit load above)
+		mla		r7,r2,r8,r7								@ += sample * ic[0]		  (now use available 16-bit loaded value)
 		mla		r7,r3,r9,r7								@ += h[0] * ic[1]
-
 		mla		r7,r5,r11,r7							@ += o[0] * oc[0]
 		mla		r7,r6,r12,r7							@ += o[1] * oc[1]
 
 		add		r7, r7, #ROUNDING_OFFSET				@ round new_sample
 		mov		r7, r7, asr #PRECISION
 
-		strh	r7,[r0],#2
-		strh	r7,[r0],#2
+		strh	r7,[r0], #2
+		strh	r7,[r0], #2
 
 		bne		.L0										@ checks r1 for zero
 
